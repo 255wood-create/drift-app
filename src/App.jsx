3ecb4c0a87d45c13d66ae348864a3363005e9f76
Lindsay Scott <255wood@gmail.com>
2026-08-05 13:50:06 -0600

    Reword disclaimer to 'pretty good...not perfect'

--- src/App.jsx
+++ src/App.jsx
@@ -491,7 +491,7 @@ export default function App(){
                 <p style={{fontFamily:"'Inter',sans-serif",fontSize:13,color:T.sage}}>Try a different filter or category</p>
               </div>
             )}
-            <p style={{fontFamily:"'Inter',sans-serif",fontSize:11,color:T.sage,textAlign:"center",marginTop:24,padding:"0 10px"}}>Please verify dates, times and locations before heading out. We ain't always perfect.</p>
+            <p style={{fontFamily:"'Inter',sans-serif",fontSize:11,color:T.sage,textAlign:"center",marginTop:24,padding:"0 10px"}}>Please verify dates, times and locations before heading out. We're pretty good...not perfect.</p>
           </main>
         )}
 
